a69000d048b1ec125a1df4caa0d633fbac6ad1e8
Alexander Soroka <sorokaa6102@gmail.com>
2025-10-23 08:44:28 +0300

    delete useless project

--- ApiProject/ApiProject/Program.cs
+++ ApiProject/ApiProject/Program.cs
@@ -1,3 +1,4 @@
+using ApiProject.Model;
 using CreateDBProj;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
@@ -7,8 +8,14 @@ using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-string connection = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(9, 4, 0))));
+//string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+DotNetEnv.Env.Load();
+var server = Environment.GetEnvironmentVariable("DB_SERVER");
+var user = Environment.GetEnvironmentVariable("DB_USER");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var connectionString = $"server={server};user={user};password={password};database={dbName}";
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseMySql(connectionString, new MySqlServerVersion(new Version(9, 4, 0))));
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
